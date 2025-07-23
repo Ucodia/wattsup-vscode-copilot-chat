c730215e0b3d0f36bea87d74fdfe7468f44c0382
Lionel Ringenbach <ucodia@live.com>
2025-07-22 18:02:12 -0700

    Added request capture example to prepare integration of existing backend

--- src/wattsup/wattsupDashboard.ts
+++ src/wattsup/wattsupDashboard.ts
@@ -3,18 +3,24 @@ import path from 'path';
 import * as vscode from 'vscode';
 import { IExtensionContribution } from '../extension/common/contributions';
 import { IVSCodeExtensionContext } from '../platform/extContext/common/extensionContext';
+import { IRequestLogger } from '../platform/requestLogger/node/requestLogger';
 import { Disposable } from '../util/vs/base/common/lifecycle';
 import equivalencesData from './equivalences.json';
-
 
 export class WattsupDashboard extends Disposable implements vscode.WebviewViewProvider, IExtensionContribution {
 	readonly id = 'wattsupDashboard';
 	private _context: IVSCodeExtensionContext;
 
-	constructor(@IVSCodeExtensionContext private readonly context: IVSCodeExtensionContext) {
+	constructor(@IVSCodeExtensionContext private readonly context: IVSCodeExtensionContext,
+		@IRequestLogger requestLogger: IRequestLogger
+	) {
 		super();
 		this._context = context;
 		this._register(vscode.window.registerWebviewViewProvider('copilot-wattsup', this));
+		this._register(requestLogger.onDidChangeRequests(async () => {
+			const requests = await requestLogger.getRequests();
+			console.log(`[wattsup] total requests logged: ${requests.length}`);
+		}));
 	}
 
 	resolveWebviewView(webviewView: vscode.WebviewView): void {
